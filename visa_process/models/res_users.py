626a29fc2de1dd6e8dae1b5c111c97949171cb5d
Deekshithhm27 <deekshith.h@lucidspire.com>
2023-11-01 19:05:21 +0530

    disabled create option in res_users for company and project manager

--- visa_process/models/res_users.py
+++ visa_process/models/res_users.py
@@ -12,6 +12,7 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     user_type = fields.Selection([('external','External'),('internal','Internal')],string="Type of user to set System Access",required=True)
+    internal_company_id = fields.Many2one('res.company',string="Company")
     partner_company_id = fields.Many2one('res.partner',string="Company",domain="[('is_company','=',True)]")
 
     company_spoc_id = fields.Many2one('hr.employee',string="Project Manager",domain="[('custom_employee_type','=','internal')]")
@@ -24,3 +25,17 @@ class ResUsers(models.Model):
             company_id=self.env.company.id,
             **self.env['hr.employee']._sync_user(self)
         ))
+
+    @api.onchange('internal_company_id')
+    def update_user_company_id(self):
+        # this method is used to update the parent_id of internal user
+        # parent_company_id is the value which is being fetched in res.partner which depends on user_ids
+        # so in this method, we are directly updating the internal user's company to parent_company_id
+        for line in self:
+            line.partner_company_id = line.internal_company_id.partner_id.id
+
+    @api.onchange('user_type')
+    def reset_company_id(self):
+        for line in self:
+            if line.user_type == 'external':
+                line.internal_company_id = False
