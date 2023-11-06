fd360b9ce3d6560333053e64236928dcaf3e80fe
Deekshithhm27 <deekshith.h@lucidspire.com>
2023-11-07 01:31:19 +0530

    Added logic to update salary amount on change of employee

--- aamalcom_payroll/models/employement_visa.py
+++ aamalcom_payroll/models/employement_visa.py
@@ -29,3 +29,27 @@ class EmploymentVisa(models.Model):
 
 
     client_salary_rule_ids = fields.One2many('salary.line', 'emp_visa_id', string="Salary Structure")
+
+
+    @api.onchange('employee_id')
+    def _onchange_employee_id(self):
+        # this method is used to update salary details from employee master
+        if self.employee_id:
+            employee = self.employee_id
+
+            # Loop through the client_salary_rule_ids in hr.employee
+            for employee_salary_line in employee.client_salary_rule_ids:
+                # Search for an existing record in local.transfer with the same name
+                existing_record = self.client_salary_rule_ids.filtered(
+                    lambda x: x.name == employee_salary_line.name
+                )
+
+                if existing_record:
+                    # Update the amount of the existing record
+                    existing_record.amount = employee_salary_line.amount
+                else:
+                    # If no existing record with the same name is found, create a new one
+                    self.client_salary_rule_ids = [(0, 0, {
+                        'name': employee_salary_line.name,
+                        'amount': employee_salary_line.amount,
+                    })]
